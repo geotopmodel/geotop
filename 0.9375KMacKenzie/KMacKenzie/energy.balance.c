6524ecd3f64d38ba322f0246f21d17fc82cb5157
matteod <matteod@931d7139-83c6-4967-8b30-00c4127a089a>
2009-10-30 10:08:32 +0000

    GEOtopKMacKenzie: inserted check to evaluate why diag[1]=0 in energy.balance
    git-svn-id: https://dev.fsc.bz.it/private/repos/geotop/trunk@169 931d7139-83c6-4967-8b30-00c4127a089a

--- 0.9375KMacKenzie/KMacKenzie/energy.balance.c
+++ 0.9375KMacKenzie/KMacKenzie/energy.balance.c
@@ -1795,6 +1795,10 @@ void PointEnergyBalance(long r,			long c,			long ns,		long ng,		double zmu,		dou
 			ad->co[l] += (C+C1)*D[l]/par->Dt;
 			b->co[l] += ((C+C1)*D[l]*T[l] - dU)/par->Dt;
 
+			if(ad->co[1]==0.0){
+				printf("r=%ld, c=%ld, ns+ng=%ld, wi[1]=%f, wl[1]=%f, dw[1]=%f, D[1]=%f C1=%f, C0=%f\n",r, c, ns+ng, wi[1], wl[1], dw[1], D[1], C1, C0);
+				t_error("Energy balance: ad[1]=0.0");
+			}
 		}
 
 		tridiag(1, r, c, n, adi, ad, ads, b, e);
