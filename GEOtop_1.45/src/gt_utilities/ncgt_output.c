56d55bfb6291f833e5e34a228c5f2c29d642a4fe
matteod <matteod@931d7139-83c6-4967-8b30-00c4127a089a>
2011-12-06 14:33:08 +0000

    added #includes
    git-svn-id: https://dev.fsc.bz.it/private/repos/geotop/trunk@555 931d7139-83c6-4967-8b30-00c4127a089a

--- GEOtop_1.45/src/gt_utilities/ncgt_output.c
+++ GEOtop_1.45/src/gt_utilities/ncgt_output.c
@@ -1,6 +1,8 @@
 #ifdef USE_NETCDF_ONGOING
 #include "../libraries/fluidturtle/turtle.h"
-#include "ncgt_output.h"
+#include "../libraries/fluidturtle/t_utilities.h"
+#include "../libraries/ascii/init.h"
+//#include "ncgt_output.h"
 #include "ncgt_turtle2netcdf.h"
 #include "gt_symbols.h"
 
