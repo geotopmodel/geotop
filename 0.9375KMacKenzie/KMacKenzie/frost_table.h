4531afe36a49103d2ba22717921a1403ba3eda3c
Antonio Messina <antonio.s.messina@gmail.com>
2013-07-04 13:36:20 +0200

    Reorder montebello branch

--- 0.9375KMacKenzie/KMacKenzie/frost_table.h
+++
@@ -1,4 +0,0 @@
-
-double find_activelayer(DOUBLEVECTOR *csi, double *d);
-double find_watertable(DOUBLEVECTOR *csi, double *d);
-double find_c(long indx, double *d);
--- GEOtop_1.45/src/libraries/geomorphology/dtm_resolution.h
+++
@@ -1,8 +0,0 @@
-#ifndef DTM_RESOLUTION_H
-#define DTM_RESOLUTION_H
-#include "../fluidturtle/turtle.h"
-
-void reduce_resolution(long n, DOUBLEMATRIX *DTM1, DOUBLEMATRIX *DTM2, T_INIT *UV1, T_INIT *UV2);
-
-void amplify_resolution(long n, long Rr, long Rc, DOUBLEMATRIX *DTM1, DOUBLEMATRIX *DTM2, T_INIT *UV1, T_INIT *UV2);
-#endif
