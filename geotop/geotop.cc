486294f7513f89000d0289d81a90dda077d9aaeb
Matteo Dall'Amico <matteodallamico@gmail.com>
2013-10-15 11:26:15 +0200

    modified geotop.cc to allow example generic_1

--- geotop/geotop.cc
+++ geotop/geotop.cc
@@ -396,7 +396,7 @@ void time_loop(AllData *A, mio::IOManager& iomanager){
 					std::vector<mio::MeteoData> vec_meteo;
 					mio::Date d1;
 					d1.setMatlabDate(JDb, TZ); // GEOtop use matlab offset of julian date
-					iomanager.getMeteoData(d1, vec_meteo);
+					if(A->M->st->Z.size()>2) iomanager.getMeteoData(d1, vec_meteo);
 
 #ifndef USE_INTERNAL_METEODISTR
 					A->P->use_meteoio_cloud = iswr_present(vec_meteo, (JDb == A->P->init_date[i_sim]), A);
