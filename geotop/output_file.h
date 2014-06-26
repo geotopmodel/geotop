69c1b3e31a04840ca2e710b02abc4bffbee665b6
Gianfranco Gallizia <skyglobe83@gmail.com>
2014-06-26 13:49:55 +0200

    Added output_file.* as dependency of inputKeywords

---
+++ geotop/output_file.h
@@ -0,0 +1,87 @@
+/* STATEMENT:
+
+   GEOtop MODELS THE ENERGY AND WATER FLUXES AT THE LAND SURFACE
+   GEOtop 2.0.0 - 9 Mar 2012
+
+   Copyright (c), 2012 - Stefano Endrizzi 
+
+   This file is part of GEOtop 2.0.0 
+
+   GEOtop 2.0.0  is a free software and is distributed under GNU General Public License v. 3.0 <http://www.gnu.org/licenses/>
+   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+
+   GEOtop 2.0.0  is distributed as a free software in the hope to create and support a community of developers and users that constructively interact.
+   If you just use the code, please give feedback to the authors and the community.
+   Any way you use the model, may be the most trivial one, is significantly helpful for the future development of the GEOtop model. Any feedback will be highly appreciated.
+
+   If you have satisfactorily used the code, please acknowledge the authors.
+
+*/
+
+/**
+ * @file output_file.h
+ * @Author  Gianfranco Gallizia (skyglobe83@gmail.com)
+ * @date June, 2014
+ * @brief Output file definition class
+ * 
+ * Defines a container for the output definition: output variable, dimension (1D, 2D, 3D),
+ * type and period of integration.
+ */
+
+#include <string>
+#include <exception>
+
+namespace geotop
+{
+    namespace input
+    {
+        enum Dimension {
+            D1Dp,
+            D1Ds,
+            D2D,
+            D3D,
+            UNKNOWN_DIM
+        };
+
+        enum IntegrationType {
+            AVG,
+            CUM,
+            INS,
+            UNKNOWN_INTEG
+        };
+
+        enum Variable {
+            UNKNOWN_VAR
+        };
+
+        class DateEur12Exception : std::exception
+        {
+        public:
+            DateEur12Exception(double date)
+            {
+                mDate = date;
+            }
+            const char* what();
+        private:
+            double mDate;
+        };
+
+        class OutputFile
+        {
+        public:
+            OutputFile(std::string extended_key, double period);
+            virtual ~OutputFile();
+            std::string getFileName(double dateeur12);
+            geotop::input::Variable getVariable() { return mVariable; }
+            geotop::input::Dimension getDimension() { return mDimension; }
+            geotop::input::IntegrationType getIntegrationType() { return mType; }
+            long getPeriod() { return mPeriod; }
+        private:
+            geotop::input::Variable mVariable;
+            geotop::input::Dimension mDimension;
+            geotop::input::IntegrationType mType;
+            long mPeriod;
+        };
+    }
+}
+
