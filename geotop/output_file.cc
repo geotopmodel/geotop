6fc633d294030dae018206cafd7f312f3f2c97c4
francescoS <francesco.serafin.3@gmail.com>
2014-09-03 09:32:59 +0200

    now log error of output variables is critical. If the variable in geotop.inpts is wrong, the program is aborted.

--- geotop/output_file.cc
+++ geotop/output_file.cc
@@ -211,9 +211,12 @@ namespace geotop
                         geotop::logger::GlobalLogger* lg =
                             geotop::logger::GlobalLogger::getInstance();
 
-                        lg->logsf(geotop::logger::WARNING,
-                                  "Unknown output variable: '%s'.",
+                        lg->logsf(geotop::logger::CRITICAL,
+                                  "Unknown output variable: '%s'. Modify the geotop.inpts. Aborting.",
                                   tmp.c_str());
+						
+						exit(1);
+						
                     }
 
                     //Dimension
@@ -446,7 +449,7 @@ namespace geotop
 
             if (tmp.compare("soiltemperature") == 0) lVar = SOIL_TEMP;
             if (tmp.compare("soilwatercontent") == 0) lVar = SOIL_WATER_CONTENT;
-
+			
             return lVar;
         }
 
