12304652499fe301a502ddbdef55f2feb8213ac3
francescoS <francesco.serafin.3@gmail.com>
2014-09-03 15:54:45 +0200

    Fixed double free bug in standard_names

--- geotop/standard_names.h
+++ geotop/standard_names.h
@@ -12,44 +12,31 @@ namespace geotop
          * @brief Output variables known
          */
         enum Variable {
-            SOIL_TEMP, //Soil temperature
+            SOIL_TEMP = 0, //Soil temperature
             SOIL_WATER_CONTENT, //Water content in soil [mm]
+            SOIL_ICE_CONTENT, //Ice content in soil depth
+            SOIL_WATER_PRESSURE, //Liquid water pressure in soil depth
+            SOIL_TOTAL_PRESSURE, //Total water and ice pressure in soil depth
             UNKNOWN_VAR
         };
 
 
-			typedef struct {
+        typedef struct {
 
-					std::string longstring;
-					std::string shortstring;
-					std::string unitstring;
-					
-			} stringpair_s;
+            std::string longstring;
+            std::string shortstring;
+            std::string unitstring;
+
+        } stringpair_s;
 
 
-			std::vector<stringpair_s> StandardNames = std::vector<stringpair_s>((size_t)UNKNOWN_VAR);
+        void fillStandardNames();
 
-			static void fillStandardName(size_t index, const std::string& longstring, const std::string& shortstring, const std::string& unitstring)
-			{
-					StandardNames[index].longstring = longstring;
-					StandardNames[index].shortstring = shortstring;
-					StandardNames[index].unitstring = unitstring;
-			}
+        std::string getLongString(Variable v);
+        std::string getShortString(Variable v);
+        std::string getUnitString(Variable v);
 
-			void fillStandardNames()
-			{
-					static bool init = false;
-
-					//Avoid multiple initializations
-					if (not init)
-					{
-							fillStandardName(SOIL_TEMP, "SoilTemperature", "SoilT", "C");
-							fillStandardName(SOIL_WATER_CONTENT, "SoilWaterContent", "Theta", "mm");
-							init = true;
-					}
-			}
-
-	}
+    }
 }
 
 #endif
