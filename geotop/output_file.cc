8aa50965de15894bc18bcec9c2bab97fd31ee73a
francescoS <francesco.serafin.3@gmail.com>
2014-09-03 10:37:30 +0200

    Fixed formatting

--- geotop/output_file.cc
+++ geotop/output_file.cc
@@ -219,9 +219,9 @@ namespace geotop
                         lg->logsf(geotop::logger::CRITICAL,
                                   "Unknown output variable: '%s'. Modify the geotop.inpts. Aborting.",
                                   tmp.c_str());
-						
+
 						exit(1);
-						
+
                     }
 
                     //Dimension
@@ -455,7 +455,7 @@ namespace geotop
 
             if (tmp.compare("soiltemperature") == 0) lVar = SOIL_TEMP;
             if (tmp.compare("soilwatercontent") == 0) lVar = SOIL_WATER_CONTENT;
-			
+
             return lVar;
         }
 
