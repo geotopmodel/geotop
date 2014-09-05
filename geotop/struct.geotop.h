6a6e5ee14fd8b831492171e6dd7ca47310a80ff5
Gianfranco Gallizia <skyglobe83@gmail.com>
2014-09-05 15:39:18 +0200

    Moved Snow class in a separate directory.

--- geotop/struct.geotop.h
+++ geotop/struct.geotop.h
@@ -22,6 +22,7 @@ If you have satisfactorily used the code, please acknowledge the authors.
 #define STRUCT_GEOTOP_H
 #include "datastructs.h"
 #include "statevar.h"
+#include "Snow/snow_class.h"
 
 #include <vector>
 
@@ -617,32 +618,6 @@ class Par
 
 };
 
-class Snow
-{
-    public:
-        Statevar3D *S;
-        Statevar1D *S_for_BS;
-        GeoVector<double> age;
-        GeoVector<double> MELTED;
-        GeoVector<double> melted;
-        GeoVector<double> HNcum;                  // TODO mattiu
-        GeoVector<double> SUBL;
-        GeoVector<double> subl;
-        GeoVector<double> t_snow;
-        GeoVector<short> yes;
-        GeoMatrix<double> Qsub;
-        GeoMatrix<double> Qsub_x;
-        GeoMatrix<double> Qsub_y;
-        GeoMatrix<double> Nabla2_Qtrans;
-        GeoMatrix<double> Qtrans;
-        GeoMatrix<double> Qsalt;
-        GeoMatrix<double> Qtrans_x;
-        GeoMatrix<double> Qtrans_y;
-        GeoMatrix<double> Wsubl_plot;
-        GeoMatrix<double> Wtrans_plot;
-        GeoVector<double> Dplot;
-        GeoVector<long> change_dir_wind;
-};
 
 class Glacier
 {
