2a70ef287ab50be0b71c653f3f3ea17e22d0d716
Gianfranco Gallizia <skyglobe83@gmail.com>
2014-09-05 12:53:56 +0200

    Added Statevar1D constructor.

--- geotop/statevar.h
+++ geotop/statevar.h
@@ -10,28 +10,31 @@
 
 class Statevar3D
 {
-    public:
-        GeoMatrix<short> type;
-        GeoMatrix<long> lnum;
-        GeoTensor<double> Dzl;
-        GeoTensor<double> w_liq;
-        GeoTensor<double> w_ice;
-        GeoTensor<double> T;
-        Statevar3D() {};
-        Statevar3D(double novalue, size_t layers, size_t rows, size_t columns);
+public:
+    GeoMatrix<short> type;
+    GeoMatrix<long> lnum;
+    GeoTensor<double> Dzl;
+    GeoTensor<double> w_liq;
+    GeoTensor<double> w_ice;
+    GeoTensor<double> T;
+    Statevar3D() {};
+    Statevar3D(double novalue, size_t layers, size_t rows, size_t columns);
 };
 
 class Statevar1D
 {
-    public:
-        short type;
-        long lnum;
-        GeoVector<double> Dzl;
-        GeoVector<double>  w_liq;
-        GeoVector<double> w_ice;
-        GeoVector<double> T;
+public:
+    short type;
+    long lnum; //TODO: check this and see if it can be changed to private
+    GeoVector<double> Dzl;
+    GeoVector<double> w_liq;
+    GeoVector<double> w_ice;
+    GeoVector<double> T;
+    Statevar1D() {};
+    Statevar1D(double novalue, size_t layers);
 };
 
 
 #endif
+
 
