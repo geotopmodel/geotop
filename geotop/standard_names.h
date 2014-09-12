dcad6ee203c401aae1cffd9731a24c7f367c973c
francescoS <francesco.serafin.3@gmail.com>
2014-09-12 12:18:38 +0200

    Put in order some comments

--- geotop/standard_names.h
+++ geotop/standard_names.h
@@ -12,31 +12,31 @@ namespace geotop
          * @brief Output variables known
          */
         enum Variable {
-            SOIL_TEMP = 0, //Soil temperature
-            SOIL_WATER_CONTENT, //Water content in soil [mm]
-            SOIL_ICE_CONTENT, //Ice content in soil depth
+            SOIL_TEMP = 0,       //Soil temperature
+            SOIL_WATER_CONTENT,  //Water content in soil [mm]
+            SOIL_ICE_CONTENT,    //Ice content in soil depth
             SOIL_WATER_PRESSURE, //Liquid water pressure in soil depth
             SOIL_TOTAL_PRESSURE, //Total water and ice pressure in soil depth
             // SNOW MAP VARIABLES
-            SNOW_AGE, //Age of the snow
-            SNOW_DEPTH, //Depth of the snow
-            SNOW_HN, //The height of the new snow fallend in the time interval
-            SNOW_MELTED, //Snowmelt
-            SNOW_SUBL, //The sublimation of the snow
-            SNOW_DURATION, //The duration of the snow
-            // SNOW_CA, //Snow covered area
+            SNOW_AGE,            //Age of the snow
+            SNOW_DEPTH,          //Depth of the snow
+            SNOW_HN,             //The height of the new snow fallend in the time interval
+            SNOW_MELTED,         //Snowmelt
+            SNOW_SUBL,           //The sublimation of the snow
+            SNOW_DURATION,       //The duration of the snow
+            // SNOW_CA,          //Snow covered area
             // WATER MAPS VARIABLES
-            PREC_TOTAL, //Total precipitation (snow + rain)
-            PREC_LIQ, //Liquid precipitation which reaches the soil surface in the time interval
-            PREC_SNOW, //Snowy part of the total precipitation
+            PREC_TOTAL,          //Total precipitation (snow + rain)
+            PREC_LIQ,            //Liquid precipitation in the time interval
+            PREC_SNOW,           //Snowy part of the total precipitation
             // ENERGY MAPS VARIABLES
-            ENER_LWin, // Incoming longwave radiation
-            ENER_SW, // Shortwave radiation
-            ENER_LW, // Longwave radiation
-            ENER_LE, // Surface latent heat flux
-            ENER_H, // Surface sensible heat flux
-            ENER_G, // Surface heat flux
-            ENER_Ts, // Surface temperature
+            ENER_LWin,           // Incoming longwave radiation
+            ENER_SW,             // Shortwave radiation
+            ENER_LW,             // Longwave radiation
+            ENER_LE,             // Surface latent heat flux
+            ENER_H,              // Surface sensible heat flux
+            ENER_G,              // Surface heat flux
+            ENER_Ts,             // Surface temperature
             VECTOR_TEST,
             UNKNOWN_VAR
         };
