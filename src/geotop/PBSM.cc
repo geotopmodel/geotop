01222e95440cabb560903f6bbf7c1e042b33aa24
stefano cozzini <cozzini@exact-lab.it>
2017-01-02 15:27:15 +0100

    some cosmetic arrangements

--- src/geotop/PBSM.cc
+++ src/geotop/PBSM.cc
@@ -1,9 +1,26 @@
-/*
+/* STATEMENT:
  
- PRAIRIE BLOWING SNOW MODEL CODE
+ GEOtop MODELS THE ENERGY AND WATER FLUXES AT THE LAND SURFACE
+ GEOtop 2.1 release candidate  (release date: 31 december 2016)
+ 
+ Copyright (c), 2016 - GEOtop Foundation
+ 
+ This file is part of GEOtop 2.1
+ 
+ GEOtop 2.1  is a free software and is distributed under GNU General Public License v. 3.0 <http://www.gnu.org/licenses/>
+ WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+ 
+ GEOtop 2.1  is distributed as a free software in the hope to create and support a community of developers and users that constructively interact.
+ If you just use the code, please give feedback to the authors and the community.
+ Any way you use the model, may be the most trivial one, is significantly helpful for the future development of the GEOtop model. Any feedback will be highly appreciated.
+ 
+ If you have satisfactorily used the code, please acknowledge the authors.
+ 
+ 
+ @brief PRAIRIE BLOWING SNOW MODEL CODE
  
  Code written by Stefano Endrizzi by translating and adapting the idea behind the PBSM Fortran Code 
- by John Pomeroy. The author does not guarantee the perfect conformance of this code with the Fortran one. 
+ by John Pomeroy. The author does not guarantee the perfect conformance of this code with the Fortran one.
  However, he asks to give credit to Pomeroy, 1993, when using it with satisfaction. 
  
  Reference:
@@ -11,6 +28,7 @@
  The Prairie Blowing Snow Model: characteristics, validation, operation
  Journal of Hydrology, 144 (1993) 165-192
  */
+
 #include "PBSM.h"
 #include "geotop_common.h"
 #include "global_logger.h"
@@ -38,7 +56,7 @@ static float CC3;
 static float CC4;
 static float CC5;
 
-//Prarie Blowing Snow Model - Pomeroy et al. (1993)
+/// @brief Prarie Blowing Snow Model - Pomeroy et al. (1993)
 
 void Pbsm (long r, long c, double Fetch, double N, double dv, double Hv, double rho_sn, double zmeas, double V, double Ta, double RH, 
 		   double *Trans, double *Subl, double *Salt, double Dsnow, double slope){
@@ -284,7 +302,4 @@ double sublimation(double Z){
 	
 }
 
-/******************************************************************************************************************************************/
-/******************************************************************************************************************************************/
-/******************************************************************************************************************************************/
-/******************************************************************************************************************************************/				
+		
--- src/geotop/parameters.cc
+++ src/geotop/parameters.cc
@@ -1,13 +1,23 @@
- /*//3.0 <http://www.gnu.org/licenses/>
+/* STATEMENT:
+ 
+ GEOtop MODELS THE ENERGY AND WATER FLUXES AT THE LAND SURFACE
+ GEOtop 2.1 release candidate  (release date: 31 december 2016)
+ 
+ Copyright (c), 2016 - GEOtop Foundation
+ 
+ This file is part of GEOtop 2.1
+ 
+ GEOtop 2.1  is a free software and is distributed under GNU General Public License v. 3.0 <http://www.gnu.org/licenses/>
  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
  
-GEOtop 2.1
+ GEOtop 2.1  is distributed as a free software in the hope to create and support a community of developers and users that constructively interact.
  If you just use the code, please give feedback to the authors and the community.
- Any way you use the model, may be the most trivial one, is significantly helpful for the future development of the Geotop model. Any feedback will be highly appreciated.
+ Any way you use the model, may be the most trivial one, is significantly helpful for the future development of the GEOtop model. Any feedback will be highly appreciated.
  
  If you have satisfactorily used the code, please acknowledge the authors.
  
  */
+
 
 #include "parameters.h"
 #include "constants.h"
@@ -29,7 +39,6 @@ using namespace boost::assign ;
 
 
 static void assign_numeric_parameters(Par *par, Land *land, Times *times, Soil *sl, Meteo *met, InitTools *itools);
-
 
 /***********************************************************/
 /***********************************************************/
