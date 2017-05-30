f95fc266828e701d42866c3608072a519a2e4bcb
Senoner Samuel <samuel.senoner@eurac.edu>
2017-05-30 16:41:31 +0200

    correcting typo

--- src/geotop/vegetation.c
+++ src/geotop/vegetation.c
@@ -614,7 +614,7 @@ void canopy_evapotranspiration(double rbv, double Tv, double Qa, double Pa, doub
 	if(VegVPDStress == 1){
 		ea = VapPressurefromSpecHumidity(Qa, Pa);
 		ev = SatVapPressure(Tv, Pa);
-		if ev-ea>VpdvegMax {
+		if(ev-ea>VpdvegMax) {
 		fe=0;
 		}else{
 		fe=1.0-(ev-ea)/VpdvegMax;
