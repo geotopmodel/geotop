dc3ac8811561c5c4df3e7ba93667d3cef80105cd
ElisaBortoli <elisa.bortoli3@gmail.com>
2018-07-03 11:11:48 +0200

    modifing matrix.h

--- src/geotop/matrix.h
+++ src/geotop/matrix.h
@@ -13,25 +13,26 @@
 
 template <typename T> class Matrix {
 public:
-    /**
-  * @return the size of the matrix
-  */
-    std::size_t size() const noexcept { return _size; }
-
-    /** pointer to the first element accessible element */
-    T *begin() noexcept { return &co[nl]; }
+    /** pointer to the first accessible element */
+    T *begin() noexcept { return &co[nrl][ncl]; }
 
     /** pointer to the one-past the last element */
-    T *end() noexcept { return &co[nh + 1]; }
+    T *end() noexcept { return &co[nrh + 1][nch + 1]; }
 
     /** const pointer to the first element accessible element */
-    const T *begin() const noexcept { return &co[nl]; }
+    const T *begin() const noexcept { return &co[nrl][ncl]; }
 
     /** const pointer to the one-past the last element */
-    const T *end() const noexcept { return &co[nh + 1]; }
+    const T *end() const noexcept { return &co[nrh + 1][nch + 1]; }
 
-    /** subscripting operator */
-    T* operator[](const std::size_t i) { return &co[ncol*i]; }
+    /** subscripting operator (non-checked) */
+    T &operator[](const std::size_t i, const std::size_t j) noexcept { return co[i][j]; }
+
+    /** subscripting operator (non-checked) */
+    const T &operator[](const std::size_t i) const noexcept { return co[i][j]; }
+
+// suggested by Alberto
+ //   T *operator[](const std::size_t i) { return &co[ncol*i]; }
 
     /** destructor. default is fine */
     ~Matrix() = default;
@@ -41,31 +42,31 @@ public:
 
 /**
    * constructor
-   * @param n_row number of rows
-   * @param n_col number of columns
+   * @param _nrl,_nrh lower and upper bound for rows
+   * @param _ncl,_nch lower and upper bound for columns
    */
-    Matrix(std::size_t n_row, std::size_t n_col): nrow{n_row}, ncol{n_col} {}
+    Matrix(const std::size_t _nrl = 1, const std::size_t _nrh, const std::size_t _ncl = 1, const std::size_t _nch)
+            : nrl{_nrl}, nrh{_nrh}, ncl{_ncl}, nch{_cch}, co{new T[nrh+1][nch+1]{}} {}
 
-    /** set all elements of the vector to @p v
+    /** set all elements of the vector to @p m
   * this is useful to reinizialize all the elements of the vector to zero
   * my_matrix = 0
   */
-    Matrix<T> &operator=(const T m) {
-      for (auto &x : *this)
-        x = m;
-      return *this;
-    }
+//    Matrix<T> &operator=(const T m) {
+//      for (auto &x : *this)
+//        x = m;
+//      return *this;
+//    } //    NON SO SE FUNZIONA....
+
+    /** lower bound */
+    std::size_t nrl, ncl;
+
+    /** upper bound */
+    std::size_t nrh, nch;
 
     /** the actual data */
     std::unique_ptr<T[]> co;
-    /** lower bound */
-    std::size_t nl;
-    /** upper bound */
-    std::size_t nh;
 
-
-private:
-    std::size_t nrow, ncol;
-
+//private:
 };
 #endif // GEOTOP_MATRIX_H
