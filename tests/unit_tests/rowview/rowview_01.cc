095ef3747f30205da0d776fe44c1eed606d325f2
ElisaBortoli <elisa.bortoli3@gmail.com>
2018-08-31 13:41:33 +0200

    modified the unit test on RowView<T>

--- tests/unit_tests/rowview/rowview_01.cc
+++ tests/unit_tests/rowview/rowview_01.cc
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include <matrix.h>
 
-  TEST(Matrix, SquareBrackets){
+  TEST(RowView, SquareBrackets){
   Matrix<int> m{2,2}; // 2x2 matrix
   int c{0};
    
@@ -15,7 +15,7 @@
  
 }
 
-  TEST(Matrix, RoundBrackets){
+  TEST(RowView, RoundBrackets){
   Matrix<int> m{2,2}; // 2x2 matrix
   int c{0};
    
@@ -27,4 +27,21 @@
   EXPECT_EQ( 3, m.row(2)(1) );
   EXPECT_EQ( 4, m.row(2)(2) );
  
+}
+
+TEST(RowView, RowView_CheckIndex){
+  Matrix<int> m{2,2}; // 2x2 matrix
+
+  EXPECT_NO_THROW( m.row(1)(1) );
+  EXPECT_NO_THROW( m.row(2)(2) );
+
+#ifndef NDEBUG
+  EXPECT_ANY_THROW( m.row(1)(0) );
+  EXPECT_ANY_THROW( m.row(2)(3) );
+
+#else
+  EXPECT_NO_THROW( m.row(1)(0) );
+  EXPECT_NO_THROW( m.row(1)(3) );
+#endif
+    
 }
