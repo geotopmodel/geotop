36489938dfd456313eecc571dda85fd5e63d3d27
ElisaBortoli <elisa.bortoli3@gmail.com>
2018-09-11 11:43:14 +0200

    deleted MATRIX from unused functions in dtm_resolution

--- src/libraries/geomorphology/dtm_resolution.h
+++ src/libraries/geomorphology/dtm_resolution.h
@@ -1,6 +1,2 @@
 
-void reduce_resolution(long n, DOUBLEMATRIX *DTM1, DOUBLEMATRIX *DTM2,
-                       T_INIT *UV1, T_INIT *UV2);
 
-void amplify_resolution(long n, long Rr, long Rc, DOUBLEMATRIX *DTM1,
-                        DOUBLEMATRIX *DTM2, T_INIT *UV1, T_INIT *UV2);
