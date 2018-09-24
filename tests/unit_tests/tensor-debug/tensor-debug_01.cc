8c5566508dbfa6295c11ff07ce283735a8fca043
ElisaBortoli <elisa.bortoli3@gmail.com>
2018-09-24 21:43:53 +0200

    printing variables type

--- tests/unit_tests/tensor-debug/tensor-debug_01.cc
+++ tests/unit_tests/tensor-debug/tensor-debug_01.cc
@@ -5,33 +5,31 @@
 
 #include <turtle.h> // for DOUBLETENSOR, includes:
 // #include "t_alloc.h" // for new_doubletensor
-// #include "t_io.h"
-// #include "t_list.h"
 
-//#include <tensor3D.h> // for initialize_doubletensor
+#include <typeinfo>
 
 // tensor(d,r,c) => d = depth; r=row; c=column
 TEST(Tensor, Tensor_value){
-  Tensor<double> t{2,2,2}; // 2x2x2 tensor
+  Tensor<double>* t = new Tensor<double>{2,2,2}; // 2x2x2 tensor
 
   double c{0.0};
-  for (auto &x : t) // need begin+end
+  for (auto &x : *t) // need begin+end
     x = ++c;
   
-  EXPECT_DOUBLE_EQ( 1.0, t(1,1,1) );
-  EXPECT_DOUBLE_EQ( 2.0, t(1,1,2) );
-  EXPECT_DOUBLE_EQ( 3.0, t(1,2,1) );
-  EXPECT_DOUBLE_EQ( 4.0, t(1,2,2) );
+  EXPECT_DOUBLE_EQ( 1.0, (*t)(1,1,1) );
+  EXPECT_DOUBLE_EQ( 2.0, (*t)(1,1,2) );
+  EXPECT_DOUBLE_EQ( 3.0, (*t)(1,2,1) );
+  EXPECT_DOUBLE_EQ( 4.0, (*t)(1,2,2) );
   
-  EXPECT_DOUBLE_EQ( 5.0, t(2,1,1) );
-  EXPECT_DOUBLE_EQ( 6.0, t(2,1,2) );
-  EXPECT_DOUBLE_EQ( 7.0, t(2,2,1) );
-  EXPECT_DOUBLE_EQ( 8.0, t(2,2,2) );
+  EXPECT_DOUBLE_EQ( 5.0, (*t)(2,1,1) );
+  EXPECT_DOUBLE_EQ( 6.0, (*t)(2,1,2) );
+  EXPECT_DOUBLE_EQ( 7.0, (*t)(2,2,1) );
+  EXPECT_DOUBLE_EQ( 8.0, (*t)(2,2,2) );
 
-  for(std::size_t k=1; k<=t.ndh; k++){
-    for(std::size_t i=1; i<=t.nrh; i++){
-      for(std::size_t j=1; j<=t.nch; j++){
-  	std::cout << t(k,i,j) << " ";
+  for(std::size_t k=1; k<=t->ndh; k++){
+    for(std::size_t i=1; i<=t->nrh; i++){
+      for(std::size_t j=1; j<=t->nch; j++){
+  	std::cout << (*t)(k,i,j) << " ";
       }
       std::cout << std::endl;
     }
@@ -74,4 +72,20 @@ TEST(Tensor, TENSOR_value){
   EXPECT_DOUBLE_EQ( 6.0, pa->co[2][1][2] );
   EXPECT_DOUBLE_EQ( 7.0, pa->co[2][2][1] );
   EXPECT_DOUBLE_EQ( 8.0, pa->co[2][2][2] );
+}
+
+TEST(Tensor, Compare_matrix){
+  Tensor<double>* t = new Tensor<double>{2,2,2}; // 2x2x2 tensor
+
+  DOUBLETENSOR *pa; // 2x2x2 tensor
+  pa = new_doubletensor(2,2,2); 
+
+  std::cout << std::endl;
+  std::cout << "pa->co[1] is a " << typeid(pa->co[1]).name() << std::endl;
+  std::cout << "t->matrix(1) is a " << typeid(t->matrix(1)).name() << std::endl;
+  std::cout << std::endl;
+  std::cout << "pa->co[1][1] is a " << typeid(pa->co[1][1]).name() << std::endl;
+  std::cout << "t->row(1,1) is a " << typeid(t->row(1,1)).name() << std::endl;
+  std::cout << std::endl;
+
 }
