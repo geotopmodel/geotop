f12d44bc2d1440c3ea30fcd0d04d9064e4a5e67e
ElisaBortoli <elisa.bortoli3@gmail.com>
2018-09-24 21:11:57 +0200

    updating unit tests for debugging tensor (pa)

--- tests/unit_tests/tensor-debug/tensor-debug_01.cc
+++ tests/unit_tests/tensor-debug/tensor-debug_01.cc
@@ -10,8 +10,6 @@
 
 //#include <tensor3D.h> // for initialize_doubletensor
 
-//#include <iostream> // prints to std::out also without it! :o
-
 // tensor(d,r,c) => d = depth; r=row; c=column
 TEST(Tensor, Tensor_value){
   Tensor<double> t{2,2,2}; // 2x2x2 tensor
@@ -29,39 +27,51 @@ TEST(Tensor, Tensor_value){
   EXPECT_DOUBLE_EQ( 6.0, t(2,1,2) );
   EXPECT_DOUBLE_EQ( 7.0, t(2,2,1) );
   EXPECT_DOUBLE_EQ( 8.0, t(2,2,2) );
+
+  for(std::size_t k=1; k<=t.ndh; k++){
+    for(std::size_t i=1; i<=t.nrh; i++){
+      for(std::size_t j=1; j<=t.nch; j++){
+  	std::cout << t(k,i,j) << " ";
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+  }
 }
 
 TEST(Tensor, TENSOR_value){
   DOUBLETENSOR *pa; // 2x2x2 tensor
   
-  std::cout << "isDynamic = " << isDynamic << std::endl;
-  std::cout << "PROVA_TURTLE = " << PROVA_TURTLE << std::endl;
-  std::cout << "PROVA_T_ALLOC = " << PROVA_T_ALLOC << std::endl;
-  
-  pa = new_doubletensor(2,2,2); // NOT working 
+  pa = new_doubletensor(2,2,2); 
 
-  // const char* c1 = "a";
-  // const char* c2 = "b";
-  // join_strings(c1,c2); // NOT working
-    
+  int idx = 1;
   
-  // for(int k=1; k<=pa->ndh; k++){
-  //   for(int i=1; i<=pa->nrh; i++){
-  //     for(int j=1; j<=pa->nch; j++){
-  // 	std::cout << pa->co[k][i][j] << " ";
-  //     }
-  //     std::cout << std::endl;
-  //   }
-  //   std::cout << std::endl;
-  // }
+  for(int k=1; k<=pa->ndh; k++){
+    for(int i=1; i<=pa->nrh; i++){
+      for(int j=1; j<=pa->nch; j++){
+  	pa->co[k][i][j] = idx;
+	idx ++;
+      }
+    }
+  }
   
-  // EXPECT_DOUBLE_EQ( 1.0, pa[1][1][1] );
-  // EXPECT_DOUBLE_EQ( 2.0, pa[1][1][2] );
-  // EXPECT_DOUBLE_EQ( 3.0, pa[1][2][1] );
-  // EXPECT_DOUBLE_EQ( 4.0, pa[1][2][2] );
+  for(int k=1; k<=pa->ndh; k++){
+    for(int i=1; i<=pa->nrh; i++){
+      for(int j=1; j<=pa->nch; j++){
+  	std::cout << pa->co[k][i][j] << " ";
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+  }
   
-  // EXPECT_DOUBLE_EQ( 5.0, pa[2][1][1] );
-  // EXPECT_DOUBLE_EQ( 6.0, pa[2][1][2] );
-  // EXPECT_DOUBLE_EQ( 7.0, pa[2][2][1] );
-  // EXPECT_DOUBLE_EQ( 8.0, pa[2][2][2] );
+  EXPECT_DOUBLE_EQ( 1.0, pa->co[1][1][1] );
+  EXPECT_DOUBLE_EQ( 2.0, pa->co[1][1][2] );
+  EXPECT_DOUBLE_EQ( 3.0, pa->co[1][2][1] );
+  EXPECT_DOUBLE_EQ( 4.0, pa->co[1][2][2] );
+  
+  EXPECT_DOUBLE_EQ( 5.0, pa->co[2][1][1] );
+  EXPECT_DOUBLE_EQ( 6.0, pa->co[2][1][2] );
+  EXPECT_DOUBLE_EQ( 7.0, pa->co[2][2][1] );
+  EXPECT_DOUBLE_EQ( 8.0, pa->co[2][2][2] );
 }
