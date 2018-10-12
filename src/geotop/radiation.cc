f26a14f1920287a87c3e3748e94a7135a8d74215
ElisaBortoli <elisa.bortoli3@gmail.com>
2018-10-12 09:35:48 +0200

    sobstituting [] with () in radiation.cc

--- src/geotop/radiation.cc
+++ src/geotop/radiation.cc
@@ -597,17 +597,17 @@ void rad_snow_absorption(long r, long c, Vector<double> *frac, double R,
           z += 0.001 * (*snow->Dzl)(l,r,c);
           rho = ((*snow->w_ice)(l,r,c) + (*snow->w_liq)(l,r,c))/ (0.001 * (*snow->Dzl)(l,r,c));
           k = rho/3.0+50.0;
-          frac->co[m] = res-R*exp(-k*z);
+         (*frac)(m) = res-R*exp(-k*z);
           res = R*exp(-k*z);
         }
 
-      frac->co[(*snow->lnum)(r,c)+1]=res;
+      (*frac)((*snow->lnum)(r,c)+1) = res;
 
     }
   else
     {
 
-      frac->co[0] = res;
+      (*frac)(0) = res;
 
     }
 }
@@ -848,8 +848,8 @@ void shadow_haiden(Matrix<double> *Z, double alpha, double direction,
 
   long nk=Nr;//y
   long nl=Nc;//x
-  double GDX=UV->U->co[2];
-  double GDY=UV->U->co[1];
+  double GDX = (*UV->U)(2);
+  double GDY = (*UV->U)(1);
 
   sx=sin(direction)*cos(alpha);
   sy=cos(direction)*cos(alpha);
