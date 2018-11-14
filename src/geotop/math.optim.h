c8b738f23ab9fa991fd44d85fc81bad070e14a47
ElisaBortoli <elisa.bortoli3@gmail.com>
2018-11-14 23:29:54 +0100

    declaration+definition of the new power in the same file

--- src/geotop/math.optim.h
+++ src/geotop/math.optim.h
@@ -1,3 +1,10 @@
 #include <cmath>
 
-double power(double a, double b);
+template <class T>
+T power(const T a, const T b){
+#ifdef MATH_OPTIM
+    return std::exp(b*std::log(a));
+#else
+    return std::pow(a,b);
+#endif
+}
