8566834815a2a21e63fbd79974d9cbc67eaa5ef7
ElisaBortoli <elisa.bortoli3@gmail.com>
2018-11-15 19:01:16 +0100

    changed pow_2(a) to avoid a NOT converging energy balance

--- src/geotop/math.optim.h
+++ src/geotop/math.optim.h
@@ -3,7 +3,15 @@
 
 #include <cmath>
 
-#define pow_2(a) a*a
+template <class T>
+T pow_2(const T a){
+#ifdef MATH_OPTIM
+return a*a;
+#else
+return std::pow(a,2);
+#endif
+}
+//#define pow_2(a) a*a
 
 template <class T>
 T power(const T a, const T b){
