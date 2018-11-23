04d6812fe51badc6e63ae00ed7da073dc3a50f23
ElisaBortoli <elisa.bortoli3@gmail.com>
2018-11-23 11:08:47 +0100

    including config.h + analyze pow results in math.optim.h

--- src/geotop/math.optim.h
+++ src/geotop/math.optim.h
@@ -1,17 +1,24 @@
 #ifndef __math_optim_h_
 #define __math_optim_h_
 
+#include "config.h"
+
 #include <cmath>
+#include <fstream>
 
 #define pow_2(a) ((a)*(a))
 
 template <class T>
 T power(const T a, const T b){
 #ifdef MATH_OPTIM
-  return std::exp(b*std::log(a));
+//    std::ofstream myfile{"analyze_power.txt", std::ios_base::app};
+//    myfile << "(" << a << "," << b << ")\t\t" << std::endl;
+    return std::exp(b*std::log(a));
 #else
-  return std::pow(a,b);
+//    std::ofstream file{"analyze_pow.txt", std::ios_base::app};
+//    file << "(" << a << "," << b << ")\t\t" << std::endl;
+    return std::pow(a,b);
 #endif
 }
 
-#endif 
+#endif
