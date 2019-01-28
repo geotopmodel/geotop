1552f303da8707c0ab440d6a70110e10d2bdf4c1
ElisaBortoli <elisa.bortoli3@gmail.com>
2019-01-28 14:31:32 +0100

    deleted post-processing file in Matsch_B2_Ref_007 test folder

--- tests/1D/Matsch_B2_Ref_007/copy-B2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR=/home/elisa/Scrivania/MHPC/geotop_3.0/tests/1D/Matsch_B2_Ref_007
-
-rm -rf ${DIR}/geotop.inpts ${DIR}/meteo0001.txt ${DIR}/*SE27XX*
-rm ${DIR}/output-tabs/*
-cp -r * ${DIR}
